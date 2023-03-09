62a642f80570d0a74c08b41732e522805912dea7
MateusEneas <mateus.eneas98@gmail.com>
2023-03-09 12:45:37 -0300

    Adicionando Operadores

--- Estudos/src/Operadores/Operadores.java
+++ Estudos/src/Operadores/Operadores.java
@@ -15,9 +15,9 @@ public class Operadores {
         System.out.println(numero);*/
 
 
-        int a = 5;
+      /*  int a = 5;
         int b = 6;
-        String resultado = a==b ? "Verdadeiro" : "Falso";
+        String resultado = a==b ? "Verdadeiro" : "Falso";*/
 
         /*if(a==b){
             resultado = "Verdadeiro";
@@ -26,7 +26,31 @@ public class Operadores {
             resultado = "Falso";
             System.out.println(resultado);
         }*/
-        System.out.println(resultado);
+    //    System.out.println(resultado);
+
+        /*int numero1 = 1;
+        int numero2 = 2;
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+        System.out.println("numeroUm é maior que o numeroDois? " + simNao);*/
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas condições são verdadeiras");
+        }else{
+            System.out.println("As duas condições são diferentes");
+        }
+
+       /* if(condicao1 || condicao2){
+            System.out.println("Uma das condições são verdadeiras!");
+        }*/
 
     }
 }
