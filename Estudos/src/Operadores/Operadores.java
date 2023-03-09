725916f1c40842d4dbc3e4fe48f8f2626aa543ac
MateusEneas <mateus.eneas98@gmail.com>
2023-03-09 12:29:59 -0300

    Adicionando Operadores

---
+++ Estudos/src/Operadores/Operadores.java
@@ -0,0 +1,31 @@
+package Operadores;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        /*int numero = 5;
+        System.out.println(numero);
+        numero = -5;
+        System.out.println(numero);*/
+
+        /*int numero = 5;
+        numero += 2;
+        System.out.println(numero);
+        numero++;
+        System.out.println(numero);*/
+
+        int a = 5;
+        int b = 6;
+        String resultado = a==b ? "Verdadeiro" : "Falso";
+
+        /*if(a==b){
+            resultado = "Verdadeiro";
+            System.out.println(resultado);
+        }else{
+            resultado = "Falso";
+            System.out.println(resultado);
+        }*/
+        System.out.println(resultado);
+
+    }
+}
---
+++ Estudos/src/TiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,24 @@
+package TiposVariaveis;
+
+public class TiposVariaveis {
+
+    public static void main(String[] args) {
+
+        String meuNome = "Mateus Eneas";
+
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        System.out.println(VALOR_DE_PI);
+        System.out.println(meuNome);
+    }
+}
